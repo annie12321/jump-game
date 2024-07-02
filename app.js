281c9e97503c6e2a6df5d27b3a15980676a2986f
Annie C. <46227477+annie12321@users.noreply.github.com>
2024-07-02 15:02:10 -0400

    Added obstacles
    Added random element of obstacles.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import Player from "./player-class.js";
 import Ground from "./ground-class.js"
+import CactiPlacer from "./cacti-placer-class.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -10,17 +11,25 @@ const GAME_SPEED_INCREMENT = 0.00001;
 // game element sizes
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 200;
-const PLAYER_WIDTH = 58;
-const PLAYER_HEIGHT = 62; // width and height should be proportional to the player png
+const PLAYER_WIDTH = 88 / 1.5;
+const PLAYER_HEIGHT = 94 / 1.5; // width and height should be proportional to the player png
 const MAX_JUMP_HEIGHT = GAME_HEIGHT;
 const MIN_JUMP_HEIGHT = GAME_HEIGHT * 0.75;
 const GROUND_WIDTH = 2400;
 const GROUND_HEIGHT = 24; // dependent on ground png
 const GROUND_AND_CACTUS_SPEED = 0.5;
 
+// obstacles array
+const CACTI_CONFIG = [
+    {width: 48 / 1.5, height: 100 / 1.5, image: "images/cactus_1.png"},
+    {width: 98 / 1.5, height: 100 / 1.5, image: "images/cactus_2.png"},
+    {width: 68 / 1.5, height: 70 / 1.5, image: "images/cactus_3.png"}
+];
+
 // game objects
 let player = null;
 let ground = null;
+let cactiPlacer = null;
 
 let scaleRatio = null;
 let previousTime = null;
@@ -52,6 +61,16 @@ function createSprites() {
         scaleRatio
     );
 
+    const cactiImages = CACTI_CONFIG.map(cactus => {
+        const image = Object.assign(new Image(), { src: cactus.image });
+        return {
+            width: cactus.width * scaleRatio,
+            height: cactus.height * scaleRatio,
+            image: image
+        };
+    })
+
+    cactiPlacer = new CactiPlacer(ctx, cactiImages, scaleRatio, GROUND_AND_CACTUS_SPEED);
 }
 
 // set up basic game screen
@@ -113,16 +132,17 @@ function gameLoop(currentTime) {
 
     const frameTimeDelta = currentTime - previousTime;
     previousTime = currentTime;
-    console.log(frameTimeDelta);
 
     clearScreen();
 
     // update game objects
     ground.update(gameSpeed, frameTimeDelta);
+    cactiPlacer.update(gameSpeed, frameTimeDelta);
     player.update(gameSpeed, frameTimeDelta);
 
     // draw game objects
     ground.draw();
+    cactiPlacer.draw();
     player.draw();
 
 
---
+++ cacti-placer-class.js
@@ -0,0 +1,66 @@
+import Cactus from "./cactus-class.js";
+
+export default class CactiPlacer {
+    CACTUS_INTERVAL_MIN = 500;
+    CACTUS_INTERVAL_MAX = 2000;
+
+    nextCactusInverval = null;
+    cacti = [];
+
+    constructor(ctx, cactiImages, scaleRatio, speed) {
+        this.ctx = ctx;
+        this.canvas = ctx.canvas;
+        this.cactiImages = cactiImages;
+        this.scaleRatio = scaleRatio;
+        this.speed = speed;
+
+        this.setNextCactusTime();
+    }
+
+    setNextCactusTime() {
+        const randNum = this.getRandomNumber(
+            this.CACTUS_INTERVAL_MIN, 
+            this.CACTUS_INTERVAL_MAX
+        );
+
+        this.nextCactusInverval = randNum;
+    }
+
+    // generate random number in interval
+    getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    createCactus() {
+        const index = this.getRandomNumber(0, this.cactiImages.length - 1);
+        const cactusImage = this.cactiImages[index];
+        const x = this.canvas.width * 1.25;
+        const y = this.canvas.height - cactusImage.height;
+        const cactus = new Cactus(
+            this.ctx, 
+            x, 
+            y, 
+            cactusImage.width, 
+            cactusImage.height, 
+            cactusImage.image
+        );
+
+        this.cacti.push(cactus);
+    }
+
+    update(gameSpeed, frameTimeDelta) { 
+        if(this.nextCactusInverval <= 0) {
+            this.createCactus();
+            this.setNextCactusTime();
+        }
+        this.nextCactusInverval -= frameTimeDelta;
+
+        this.cacti.forEach((cactus) => {
+            cactus.update(this.speed, gameSpeed, frameTimeDelta, this.scaleRatio);
+        });
+    }
+
+    draw() {
+        this.cacti.forEach((cactus) => cactus.draw());
+    }
+}
